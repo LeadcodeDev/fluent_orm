d4606a7bf379125c638efacd8d8722cfce805a7c
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-07-31 09:05:47 +0200

    feat: initial commit

---
+++ bin/fluent_orm.dart
@@ -0,0 +1,5 @@
+import 'package:fluent_orm/fluent_orm.dart' as fluent_orm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fluent_orm.calculate()}!');
+}
---
+++ test/fluent_orm_test.dart
@@ -0,0 +1,8 @@
+import 'package:fluent_orm/fluent_orm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
